e5181984ec8948f3b887a927f1b0b8952d65d5ae
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-06-13 01:29:52 +0600

    Flas templates and page layout

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+allposts = [
+    {
+        'author': "Sadhin",
+        'title': "Blog post 1",
+        'content': "First post content",
+        'date_posted': "April 20 2023"
+    },
+    {
+        'author': "Jone Doe",
+        'title': "Blog post 2",
+        'content': "Second post content",
+        'date_posted': "May 10 2023"
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template("home.html", posts=allposts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
