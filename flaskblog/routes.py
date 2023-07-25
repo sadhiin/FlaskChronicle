80813c0f74d188daa9a3c755618913f8c8e500e7
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-07-25 10:56:08 +0600

    On to the reset password feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -179,3 +179,25 @@ def user_post(username):
         .order_by(Post.date_posted.desc()) \
         .paginate(page=page, per_page=2)
     return render_template("user_posts.html", posts=posts, user=user)
+
+@app.route("/reset_password", methods=["GET","POST"])
+def reset_request():
+    if current_user.is_authenticated:
+        # flash("You already logedin", 'warning')
+        return redirect(url_for('home'))
+
+    form = RequestResetForm()
+    return render_template('reset_resquest.html', title= "Reset Password", form=form)
+
+
+
+@app.route("/reset_password/<token>", methods=["GET","POST"])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is a invalid or expired token", "warning")
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template("reset_token.html", title="Reset Password", form = form)
