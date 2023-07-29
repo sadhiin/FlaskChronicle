f62af798c55145eea70b262bda90815e3d238fbd
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-07-29 12:06:26 +0600

    Applying Flask-Blueprint

---
+++ flaskblog/users/routes.py
@@ -0,0 +1,133 @@
+from flask import render_template, redirect, request, Blueprint, url_for, flash
+from flask_login import login_user, current_user, logout_user, login_required
+from flaskblog import db, bcrypt
+from flaskblog.models import User, Post
+
+from flaskblog.users.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
+from flaskblog.users.utils import save_picture, send_reset_email, delete_previous_img
+
+# from flaskblog.users.
+
+users = Blueprint("users", __name__)
+
+
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        flash("You already logedin", 'warning')
+        return redirect(url_for('home'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(str(form.password.data)).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can login'.format(form.username.data), "success")
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash("You already logedin", 'warning')
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                # print(next_page)
+                return redirect(url_for(next_page[1:])) if next_page else redirect(url_for('home'))
+            else:
+                flash("Login Unsuccessful! Check email and password", 'danger')
+
+    return render_template("login.html", title="Login", form=form)
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            delete_previous_img(current_user.image_file)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Account Information updated!", 'success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
+
+
+# @app.route("user/<str:username>")
+# def user_post(username):
+#         page = request.args.get("page", 1, type=int)
+#         user = User.query.filter_by(username= username).first_or_404()
+#         posts = Post.query.filter_by(author = user)\
+#                 .order_by(Post.date_posted.desc())\
+#                 .paginate(page=page, per_page=2)
+#         return render_template("user_posts.html", posts=posts, user=user)
+#
+
+@users.route("/user/<string:username>")
+def user_post(username):
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user) \
+        .order_by(Post.date_posted.desc()) \
+        .paginate(page=page, per_page=2)
+    return render_template("user_posts.html", posts=posts, user=user)
+
+
+@users.route("/reset_password", methods=["GET", "POST"])
+def reset_request():
+    if current_user.is_authenticated:
+        # flash("You already logedin", 'warning')
+        return redirect(url_for('home'))
+
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash("Email has been send. Check you Inbox!", "info")
+        return redirect("login")
+
+    return render_template('reset_resquest.html', title="Reset Password", form=form)
+
+
+@users.route("/reset_password/<token>", methods=["GET", "POST"])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is a invalid or expired token", "warning")
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(str(form.password.data)).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been Updated! You can login', "success")
+        return redirect(url_for('login'))
+    return render_template("reset_token.html", title="Reset Password", form=form)
